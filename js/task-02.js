88105c741fa4bdeb2b7a965e4057c95fa018915d
Ilya Kaplan <71782985+IlyaKaplan2002@users.noreply.github.com>
2021-10-27 19:31:45 +0300

    task-02 fixed

--- js/task-02.js
+++ js/task-02.js
@@ -8,20 +8,29 @@ const ingredients = [
 ];
 
 function makeMarkup({ tagName, content, className }) {
-  const elem = document.createElement(tagName);
-  elem.textContent = content;
-  elem.classList.add(className);
-  return elem;
+  // const elem = document.createElement(tagName);
+  // elem.textContent = content;
+  // elem.classList.add(className);
+  // return elem;
+  return `<${tagName} class='${className}'>${content}</${tagName}>`;
 }
 
 const list = document.querySelector("ul#ingredients");
 
+let markup = "";
 for (const ingredient of ingredients) {
-  list.append(
-    makeMarkup({
-      tagName: "li",
-      content: ingredient,
-      className: "item",
-    })
-  );
+  // list.append(
+  //   makeMarkup({
+  //     tagName: "li",
+  //     content: ingredient,
+  //     className: "item",
+  //   })
+  // );
+  markup += makeMarkup({
+    tagName: "li",
+    content: ingredient,
+    className: "item",
+  });
 }
+
+list.innerHTML = markup;
